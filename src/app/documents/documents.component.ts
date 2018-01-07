958baa369f3a6b1ccab64cf69d240ad3f932748f
flouu <31997028+flouu@users.noreply.github.com>
2018-01-07 23:03:31 +0200

    implemented mock data for documents

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,10 +1,32 @@
 import { Component } from "@angular/core";
-
+import { Document } from "./document";
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-  
+  documents: Document[] = [
+    {
+      title:"My First Doc",
+      description: 'Placeholder description',
+      file_url: 'http://google.com',
+      updated_at: '09/11/16',
+      image_url: 'http://google.com',
+    },
+    {
+      title:"My Second Doc",
+      description: 'Placeholder description',
+      file_url: 'http://google.com',
+      updated_at: '10/11/16',
+      image_url: 'http://google.com',
+    },
+    {
+      title:"My Third Doc",
+      description: 'Placeholder description',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    }
+  ]
 }
